fb0e55caead713b5d111e0f40c7ef7bfa0e42340
Akash Butala <akash.butala.365@gmail.com>
2023-01-06 23:55:22 -0800

    Adding code

--- ContainsDuplicate.py
+++ ContainsDuplicate.py
@@ -10,20 +10,3 @@ for i in range(len(nums)):
         hashSet.add(nums[i])
 print("False") #return
 
-
-
-
-# flag = 0
-# if (len(set(nums))== len(nums)):
-#     print("True 1")
-# else:
-#     myDict = {i:nums.count(i) for i in nums}
-#     print(myDict)
-#     for key, value in myDict.items():
-#         if value<2:
-#             flag = 1
-#             break
-#     if flag:
-#         print("False 2")
-#     else:
-#         print("True 2") 
